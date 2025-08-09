ec455406d99f7850f7aac597ac9553a8b0147ad1
Andrii Kurlykin <andreirtt9@gmail.com>
2025-08-09 23:44:44 +0200

    LC 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include "tasks/tasks.hpp"
 
-int main(){
-    std::cout << "Hello world!" << std::endl;
-    
+int main() {
+    // std::cout << "Hello world!" << std::endl;
+
+    //1. Two Sum
+
+    std::vector a = { 3, 2, 4 };
+
+    std::vector vec = twoSum(a, 6);
+
+    // for (int el : vec) {
+    //     std::cout << el << " ";
+    // }
+
+    // C-style cast
+    std::cout << (int)56.99;
+
+
+
     return 0;
 }
---
+++ src/tasks/tasks.cpp
@@ -0,0 +1,45 @@
+#include "tasks.hpp"
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <unordered_map>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    // std::vector<int> result;
+    // for (size_t i = 0; i < nums.size(); ++i) {
+    //     for (size_t j = i + 1; j < nums.size(); ++j) {
+    //         if (nums[i] + nums[j] == target) {
+    //             result.push_back(i);
+    //             result.push_back(j);
+    //             return result;
+    //         }
+    //     }
+    // }
+    // return result;
+
+    // T 1
+    for (size_t i = 0; i < nums.size(); ++i) {
+        auto it = find(nums.begin(), nums.end(), target - nums[i]);
+        if (it != nums.end() && (it - nums.begin() != static_cast<int>(i))) {
+            return {static_cast<int>(i), static_cast<int>(it - nums.begin())};
+        }
+    }
+
+
+
+    // T 2
+    std::unordered_map<int, int> num_c;
+
+    for (size_t i = 0; i < nums.size(); ++i) {
+        int c = target - nums[i];
+
+        if (num_c.count(c)) {
+            return {static_cast<int>(num_c[c]), static_cast<int>(i)};
+        }
+
+        num_c[nums[i]] = i;
+    }
+
+    return {};
+}
+
